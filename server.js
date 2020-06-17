71a0332aa74460b98b2f6545d52266fde09550a0
Grzegorz Kupczyk <kupczykgrzeg@gmail.com>
2020-06-17 21:08:33 +0200

    Serwer WWW

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 3005;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(express.static("public"));
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
