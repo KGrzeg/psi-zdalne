43bcedb8df7efe66105b09d54cd7405d26d76c19
Grzegorz Kupczyk <kupczykgrzeg@gmail.com>
2020-06-17 20:07:34 +0200

    Zadana wartosc w czasie rzeczywistym

--- server-ethernet.js
+++ server-ethernet.js
@@ -1,13 +1,26 @@
-var net = require("net");
+const net = require("net");
+const readline = require("readline");
 
-var server = net.createServer(function (socket) {
-  const liczba = 3;
+let matlab_socket = null;
+const rl = readline.createInterface({
+  input: process.stdin,
+});
+
+function wyslij(liczba) {
   const buffor = Buffer.allocUnsafe(8);
   buffor.writeDoubleBE(liczba);
 
-  socket.write(buffor);
+  matlab_socket.write(buffor);
+}
 
+const server = net.createServer(function (socket) {
   socket.pipe(socket);
+  matlab_socket = socket;
 });
 
 server.listen(2137, "127.0.0.1");
+
+rl.on("line", (line) => {
+  liczba = parseFloat(line);
+  wyslij(liczba);
+});
