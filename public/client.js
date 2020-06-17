bdb58bf5274c28e65b147afcf8b75f252d4f660b
KGrzeg <kupczykgrzeg@gmail.com>
2020-06-17 22:16:09 +0200

    Komunikacja z matlaba do przegladarki

--- public/client.js
+++ public/client.js
@@ -3,8 +3,14 @@ const websocket = io();
 const button = document.querySelector("#wyslij");
 const zadana = document.querySelector("#zadana");
 const kp = document.querySelector("#kp");
+const odpowiedz = document.querySelector("#odpowiedz");
 
 button.addEventListener("click", () => {
   const dane = [zadana.value, kp.value];
   websocket.emit("aktualizacja", dane);
+});
+
+websocket.on("odpowiedz", (liczba) => {
+  liczba = (liczba * 10000).toFixed() / 10000;
+  odpowiedz.innerText = liczba;
 });
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
+const net = require("net");
 const express = require("express");
 const app = express();
 const port = 3005;
 
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
+
+let matlab_socket = null;
 
 app.use(express.static("public"));
 
@@ -15,6 +18,18 @@ io.on("connection", (socket) => {
   });
 });
 
+const server = net.createServer(function (socket) {
+  matlab_socket = socket;
+
+  socket.on("data", (data) => {
+    const liczba = data.readDoubleBE();
+    console.log("<<< " + liczba);
+
+    io.sockets.emit("odpowiedz", liczba);
+  });
+});
+
 http.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
 );
+server.listen(2137, "127.0.0.1");
