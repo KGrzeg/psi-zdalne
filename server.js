f39f228e54b90ed73f62950bd4f3f28a22c680a9
KGrzeg <kupczykgrzeg@gmail.com>
2020-06-17 21:38:53 +0200

    Przygotowanie serwera websocket

--- server.js
+++ server.js
@@ -2,8 +2,15 @@ const express = require("express");
 const app = express();
 const port = 3005;
 
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
 app.use(express.static("public"));
 
-app.listen(port, () =>
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
+
+http.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
 );
